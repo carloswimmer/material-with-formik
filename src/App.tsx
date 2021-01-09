e2f46ddb1270753373b177f81404fa3d3cfd74c1
Carlos Wimmer <coiote777@gmail.com>
2021-01-09 18:43:00 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppProvider from './hooks';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <h1>Material with Formik</h1>
+    </AppProvider>
   );
-}
+};
 
 export default App;
